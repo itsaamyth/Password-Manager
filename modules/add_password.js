8b90cb3909ebdde2d413bf10ea6c448ccd14000f
Amitk6228 <59211195+Amitk6228@users.noreply.github.com>
2020-09-21 20:33:40 +0530

    change in db

--- modules/add_password.js
+++ modules/add_password.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/pms',{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true})
-// mongoose.connect('mongodb+srv://Amit_Kumar:Amitk6228@cluster0.vuxfq.mongodb.net/pms?retryWrites=true&w=majority',{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true})
+// mongoose.connect('mongodb://localhost:27017/pms',{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true})
+mongoose.connect('mongodb+srv://Amit_Kumar:Amitk6228@cluster0.vuxfq.mongodb.net/pms?retryWrites=true&w=majority',{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true})
 var conn=mongoose.Collection
 var passScheme= new mongoose.Schema({
     password_category:{
